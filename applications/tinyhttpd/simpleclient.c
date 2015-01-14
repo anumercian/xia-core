0282b5521659350a2ffe3db5b59cc13e7bb6af19
barrettd <barrettd@cs.cmu.edu>
2015-01-14 16:52:12 -0500

    Changes for wrapper testing.

--- applications/tinyhttpd/simpleclient.c
+++ applications/tinyhttpd/simpleclient.c
@@ -1,51 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
-#include <stdlib.h>
 #include <netdb.h>
-#include "xia.h"
-#include "dagaddr.hpp" // only needed to pretty print the dag
-#define SID "SID:8080808080808080808080808080808080808080"
 
 int main(int argc, char *argv[])
 {
  int sockfd;
- int len;
- struct addrinfo *ai;
- int result;
- char ch = 'A';
- char buf[10000];
+ //int len;
+ //struct sockaddr_in address;
+ int rc;
+ char get_request[] = "GET /\n\n";
+ char buf[16384];
 
- sockfd = socket(AF_XIA, SOCK_STREAM, 0);
- if (getaddrinfo(NULL, SID, NULL, &ai) < 0) {
-	 printf("getaddrinfo faliure\n");
-	 return 0;
+ sockfd = socket(AF_INET, SOCK_STREAM, 0);
+ if(sockfd < 0) {
+	perror("failed to create a socket to talk to server\n");
+	return -1;
  }
+ printf("Created socket with descriptor %d\n", sockfd);
 
- Graph g((sockaddr_x*)ai->ai_addr);
- printf("%s\n", g.dag_string().c_str());
+struct addrinfo *ai;
 
- len = sizeof(sockaddr_x);
- result = connect(sockfd, ai->ai_addr, len);
+ getaddrinfo("0.0.0.0", "8080", NULL, &ai);
 
- if (result == -1)
+
+ //address.sin_family = AF_INET;
+ //address.sin_addr.s_addr = inet_addr("0.0.0.0");
+ //address.sin_port = htons(8080);
+ //len = sizeof(address);
+ printf("Connecting to server at 0.0.0.0 port 8080\n");
+ //rc = connect(sockfd, (struct sockaddr *)&address, len);
+ rc = connect(sockfd, (struct sockaddr *)ai->ai_addr, sizeof(struct sockaddr));
+ if (rc < 0)
  {
   perror("oops: client1");
-  exit(1);
+  return -1;
  }
- send(sockfd, "GET / HTTP/1.0\n\n", 16, 0);
-
- // FIXME: didn't add code to detect end of file
- while (1) {
-	int cnt = recv(sockfd, buf, sizeof(buf), 0);
-	buf[cnt] = 0;
-	printf("%s\n", buf);
+ printf("Connected to server\n");
+ rc = send(sockfd, get_request, sizeof(get_request), 0);
+ if(rc != sizeof(get_request)) {
+ 	printf("Failed to send %lu bytes containing %s\n", sizeof(get_request), get_request);
+ 	exit(1);
  }
-
+ printf("Sent %d bytes to server\n", rc);
+ rc = recv(sockfd, buf, sizeof(buf), 0);
+ if(rc < 0) {
+ 	perror("recv from server failed\n");
+ 	exit(1);
+ } else {
+ 	printf("Server returned %d bytes:\n%s|||", rc, buf);
+ }
+ printf("Disconnecting from server\n");
  close(sockfd);
- exit(0);
+ return 0;
 }
